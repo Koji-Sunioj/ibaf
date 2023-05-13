1943326e82fbb0bf48bad2830fe9cd0899f83101
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-05-13 21:03:25 +0300

    fuck eslint

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSearchParams, Link } from "react-router-dom";
 
 import Card from "react-bootstrap/Card";
