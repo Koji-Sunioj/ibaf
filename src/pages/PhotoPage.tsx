6fc8cedbdc84455341acabf0e7533763204077aa
Koji-Sunioj <koji.gabriel218@gmail.com>
2024-01-04 20:28:45 +0200

    back

--- src/pages/PhotoPage.tsx
+++ src/pages/PhotoPage.tsx
@@ -29,72 +29,84 @@ const PhotoPage = () => {
     })();
 
   return (
-    <Row>
-      <Col lg={6}>
-        {pagePhoto !== null && (
-          <Card className="mt-3">
-            <Card.Img variant="top" src={pagePhoto.file} />
-            <Card.Body>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <p>
-                  <strong>photo id: </strong>
-                  {pagePhoto.photoId}
-                </p>
-                <p>
-                  <strong>collection: </strong>
-                  <Link
-                    to={`/results?collection=${pagePhoto.collection}&type=tags`}
-                    onClick={(event) => {
-                      event.preventDefault();
+    <>
+      <button id="back-button">
+        <span
+          style={{ fontSize: "50px", lineHeight: "20px" }}
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          &#8592;
+        </span>
+      </button>
+      <Row>
+        <Col lg={6}>
+          {pagePhoto !== null && (
+            <Card className="mt-3">
+              <Card.Img variant="top" src={pagePhoto.file} />
+              <Card.Body>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <p>
+                    <strong>photo id: </strong>
+                    {pagePhoto.photoId}
+                  </p>
+                  <p>
+                    <strong>collection: </strong>
+                    <Link
+                      to={`/results?collection=${pagePhoto.collection}&type=tags`}
+                      onClick={(event) => {
+                        event.preventDefault();
+                        const params = {
+                          pathname: "/results",
+                          search: `?collection=${pagePhoto.collection}&type=tags`,
+                        };
+                        dispatch(resetFilter());
+                        dispatch(resetPhotos());
+                        navigate(params);
+                      }}
+                    >
+                      {pagePhoto.collection}
+                    </Link>
+                  </p>
+                  <p>
+                    <strong>date: </strong>
+                    {pagePhoto.date}
+                  </p>
+                </div>
+                <Card.Text>
+                  <strong>caption: </strong>
+                  {pagePhoto.caption}
+                </Card.Text>
+                {pagePhoto.tags.map((tag) => (
+                  <Button
+                    size="sm"
+                    style={{ margin: "3px 3px 3px 0px" }}
+                    onClick={() => {
                       const params = {
                         pathname: "/results",
-                        search: `?collection=${pagePhoto.collection}&type=tags`,
+                        search: `?tags=${tag}`,
                       };
-                      dispatch(resetFilter());
-                      dispatch(resetPhotos());
+                      /*   dispatch(resetFilter());
+                  dispatch(resetPhotos()); */
                       navigate(params);
                     }}
                   >
-                    {pagePhoto.collection}
-                  </Link>
-                </p>
-                <p>
-                  <strong>date: </strong>
-                  {pagePhoto.date}
-                </p>
-              </div>
-              <Card.Text>
-                <strong>caption: </strong>
-                {pagePhoto.caption}
-              </Card.Text>
-              {pagePhoto.tags.map((tag) => (
-                <Button
-                  size="sm"
-                  style={{ margin: "3px 3px 3px 0px" }}
-                  onClick={() => {
-                    const params = {
-                      pathname: "/results",
-                      search: `?collection=${pagePhoto.collection}&type=tags&query=${tag}`,
-                    };
-                    dispatch(resetFilter());
-                    dispatch(resetPhotos());
-                    navigate(params);
-                  }}
-                >
-                  {tag}
-                </Button>
-              ))}
-            </Card.Body>
-          </Card>
-        )}
-      </Col>
-    </Row>
+                    {tag}
+                  </Button>
+                ))}
+              </Card.Body>
+            </Card>
+          )}
+        </Col>
+      </Row>
+    </>
   );
 };
 
